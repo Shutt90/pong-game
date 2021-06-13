766a381244a4413c5ac3233be689a2f67b4526ca
Liam Pugh <78647249+Shutt90@users.noreply.github.com>
2021-06-13 20:40:09 +0100

    Added world bounds and started working on colliders.
    Need to set top and bottom of worlds to bounce and finish adding the sprites to colliders next.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Phaser from "phaser";
 
+let currentScore
+
 class MyGame extends Phaser.Scene {
   constructor() {
     super();
@@ -11,12 +13,16 @@ class MyGame extends Phaser.Scene {
   }
 
   create() {
-    const ball = this.add.image(400, 300, "ball");
-    const playerPaddle = this.add.image(25, 300, "paddle");
-    const enemyPaddle = this.add.image(775, 300, "paddle");
+    var ball = this.physics.add.sprite(400, 300, "ball");
+    var playerPaddle = this.physics.add.sprite(25, 300, "paddle");
+    var enemyPaddle = this.physics.add.sprite(775, 300, "paddle");
     const pause = (this.time.pause = true);
-    const playerPaddleCollide = this.object1;
-    const enemyPaddleCollide = this.object2;
+    ball.setBounce(0.2);
+    ball.setCollideWorldBounds(true);
+    playerPaddle.setCollideWorldBounds(true);
+    enemyPaddle.setCollideWorldBounds(true); //sets sprite to stay in frame/canvas
+    console.log(playerPaddle)
+    playerPaddle.body.checkCollision,{right: true};
 
     this.tweens.add({
       targets: ball,
@@ -25,7 +31,20 @@ class MyGame extends Phaser.Scene {
       ease: "Power0",
     });
 
+    // var reverseBall = function () {
+    //   if (ball.body.x === (playerPaddle.body.x - ball.body)|| ball.body.x === (playerPaddle.body.x - ball.body)) {
+    //     config.physics.arcade.velocity.x === -config.physics.arcade.velocity.x
+    //   }
+    //   currentScore++
+    // }
+    // reverseBall();
+
     console.log(this.tweens);
+  }
+
+  update() {
+    
+
   }
 }
 
@@ -39,6 +58,8 @@ const config = {
     default: "arcade",
     arcade: {
       velocity: { x: 3000 },
+      debug: true,
+      // collider: [this.playerPaddle, this.enemyPaddle]
     },
   },
 };
