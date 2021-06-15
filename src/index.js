daed95104cbb00bf8268d35c2f68cc6d0b68893f
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-06-15 11:57:51 +0100

    Refactoring/Cleanup

--- src/index.js
+++ src/index.js
@@ -12,9 +12,26 @@ class MyGame extends Phaser.Scene {
     this.load.image("ball", "./src/assets/ball2.png");
     this.load.image("paddle", "./src/assets/laser.png");
     this.load.image("background", "./src/assets/background.jpg");
+
+    this.startPositionBall = { x: 400, y: 300 };
+
+    this.startPlayPaddle = {
+      x: this.startPositionBall.x - 345,
+      y: this.startPositionBall.y,
+    };
+
+    this.startEnemyPaddle = {
+      x: this.startPositionBall.x + 345,
+      y: this.startPositionBall.y,
+    };
   }
+
   create() {
-    this.background = this.add.image(400, 300, "background");
+    this.background = this.add.image(
+      this.startPositionBall.x,
+      this.startPositionBall.y,
+      "background"
+    );
     this.background.setScale(0.2);
     this.arr = [-300, 300];
     this.scoreText1 = this.add.text(200, 300, playerCurrentScore, {
@@ -25,30 +42,45 @@ class MyGame extends Phaser.Scene {
       fontSize: "64px",
       fill: "#feb700",
     });
-    this.ball = this.physics.add.sprite(400, 300, "ball");
-    this.ball.setScale(0.6);
-    this.playerPaddle = this.physics.add.sprite(55, 300, "paddle");
-    this.enemyPaddle = this.physics.add.sprite(745, 300, "paddle");
-    this.physics.add.collider(this.ball, this.playerPaddle, null, null, this);
-    this.physics.add.collider(this.ball, this.enemyPaddle, null, null, this);
-    this.ball.setCollideWorldBounds(true);
-    this.ball.setBounce(1.2, 1.2);
+    this.ball = this.physics.add.sprite(
+      this.startPositionBall.x,
+      this.startPositionBall.y,
+      "ball"
+    );
+    this.ball;
+    this.playerPaddle = this.physics.add.sprite(
+      this.startPlayPaddle.x,
+      this.startPlayPaddle.y,
+      "paddle"
+    );
+    this.enemyPaddle = this.physics.add.sprite(
+      this.startEnemyPaddle.x,
+      this.startEnemyPaddle.y,
+      "paddle"
+    );
+
+    this.ball
+      .setCollideWorldBounds(true)
+      .setCircle(31)
+      .setMaxVelocity(1500)
+      .setBounce(1.2, 1.2)
+      .setScale(0.6);
+
     this.ball.body.allowRotation = true;
     this.ball.body.velocity.set(
       this.arr[Math.floor(Math.random() * this.arr.length)],
       this.rand
     );
-    this.playerScore = () => playerCurrentScore++;
-    this.enemyScore = () => enemyCurrentScore++;
-    this.playerPaddle.setCollideWorldBounds(true);
-    this.enemyPaddle.setCollideWorldBounds(true);
-    this.playerPaddle.setImmovable();
-    this.enemyPaddle.setImmovable();
-    this.keys = this.input.keyboard.addKeys("W,S,A,D,I,J,L,K");
-    this.ball.setCircle(31);
-
     this.ball.body.setAngularVelocity(250);
-    this.ball.setMaxVelocity(1500);
+
+    this.playerPaddle.setCollideWorldBounds(true).setImmovable();
+    this.enemyPaddle.setCollideWorldBounds(true).setImmovable();
+
+    this.physics.add.collider(this.ball, this.playerPaddle, null, null, this);
+    this.physics.add.collider(this.ball, this.enemyPaddle, null, null, this);
+
+    this.keys = this.input.keyboard.addKeys("W,S,A,D,I,J,L,K");
+
     this.timer = function () {
       this.scene.resume();
     };
@@ -64,11 +96,10 @@ class MyGame extends Phaser.Scene {
         enemyCurrentScore++;
         this.scene.restart();
         this.scene.pause();
-        console.log(this.time);
         // this.time.(4000, this.resumeGame(), [], this)
 
         // this.time.delayedCall(
-        //   1000,
+        //   3000,
         //   function () {
         //     return this.scene.resume;
         //   },
